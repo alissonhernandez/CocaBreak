1ba9e02070e88f8fdbddf39da7be6f3e1b7a38ca
Osiris30 <andradeivania16@gmail.com>
2026-06-15 21:19:58 -0600

    modificando fragment perfil, agregando iconos y creando EditarPerfilFragment

---
+++ app/src/main/java/com/example/cocabreak/fragments/EditarPerfilFragment.java
@@ -0,0 +1,12 @@
+package com.example.cocabreak.fragments;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.cocabreak.R;
+
+public class EditarPerfilFragment extends Fragment {
+
+    public EditarPerfilFragment() {
+        super(R.layout.fragment_editar_perfil);
+    }
+}
--- app/src/main/java/com/example/cocabreak/fragments/PerfilFragment.java
+++ app/src/main/java/com/example/cocabreak/fragments/PerfilFragment.java
@@ -1,5 +1,11 @@
 package com.example.cocabreak.fragments;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.cocabreak.R;
@@ -8,5 +14,38 @@ public class PerfilFragment extends Fragment {
 
     public PerfilFragment() {
         super(R.layout.fragment_perfil);
+    }
+
+    @Override
+    public void onViewCreated(
+            @NonNull View view,
+            @Nullable Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView btnMiInformacion =
+                view.findViewById(R.id.btnMiInformacion);
+
+        TextView btnCerrarSesion =
+                view.findViewById(R.id.btnCerrarSesion);
+
+        btnMiInformacion.setOnClickListener(v ->
+
+                requireActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(
+                                R.id.fragmentContainer,
+                                new EditarPerfilFragment()
+                        )
+                        .addToBackStack(null)
+                        .commit()
+        );
+
+        btnCerrarSesion.setOnClickListener(v -> {
+
+            requireActivity().finish();
+
+        });
     }
 }
